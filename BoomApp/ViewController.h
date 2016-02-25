dbbd56a870a115b63b8821e6c3ec9b91df8e2233
Christopher Augg <chrisa39@uw.edu>
2016-02-25 13:45:45 -0800

    converted swift supercool app to objective-c. This is initial commit.

---
+++ BoomApp/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  BoomApp
+//
+//  Created by Chris Augg on 2/25/16.
+//  Copyright © 2016 Auggie Doggie iOSware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *logo;
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImg;
+@property (weak, nonatomic) IBOutlet UIButton *makeMeCoolerBtn;
+
+@end
+
---
+++ BoomApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  BoomApp
+//
+//  Created by Chris Augg on 2/25/16.
+//  Copyright © 2016 Auggie Doggie iOSware. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];    
+}
+
+- (IBAction)makeMeCooler:(id)sender {
+    
+    _logo.hidden = false;
+    _backgroundImg.hidden = false;
+    _makeMeCoolerBtn.hidden = true;
+}
+
+@end
